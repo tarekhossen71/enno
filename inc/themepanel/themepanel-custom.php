c271b01e88e30e7bab7cc333f243d8c33d63c1c2
tarekhossen71 <tarekhossen.offi@gmail.com>
2022-06-24 14:40:16 +0600

    counter section done

--- inc/themepanel/themepanel-custom.php
+++ inc/themepanel/themepanel-custom.php
@@ -118,5 +118,27 @@ if( class_exists( 'CSF' ) ) {
             ),
         ),
     ) );
+    CSF::createSection( $prefix, array(
+        'title'     => 'Counter Section',
+        'fields'    => array(
+            array(
+                'id'        => 'courser_section_repeater',
+                'title'     => 'Counter',
+                'type'      => 'repeater',
+                'fields'    => array(
+                    array(
+                        'id'        => 'counter_number',
+                        'title'     => 'Count Number',
+                        'type'      => 'text',
+                    ),
+                    array(
+                        'id'        => 'counter_name',
+                        'title'     => 'Count Name',
+                        'type'      => 'text',
+                    ),
+                ),
+            ),
+        ),
+    ) );
     
   }
--- template-parts/count.php
+++ template-parts/count.php
@@ -1,28 +1,29 @@
+<?php
+    $options = get_option( 'my_framework' );
+?>
 <!-- ======= Counts Section ======= -->
 <section id="counts" class="counts">
       <div class="container">
 
         <div class="row counters">
-
-          <div class="col-lg-3 col-6 text-center">
-            <span data-purecounter-start="0" data-purecounter-end="232" data-purecounter-duration="1" class="purecounter"></span>
-            <p>Clients</p>
-          </div>
-
-          <div class="col-lg-3 col-6 text-center">
-            <span data-purecounter-start="0" data-purecounter-end="521" data-purecounter-duration="1" class="purecounter"></span>
-            <p>Projects</p>
-          </div>
-
-          <div class="col-lg-3 col-6 text-center">
-            <span data-purecounter-start="0" data-purecounter-end="1463" data-purecounter-duration="1" class="purecounter"></span>
-            <p>Hours Of Support</p>
-          </div>
-
-          <div class="col-lg-3 col-6 text-center">
-            <span data-purecounter-start="0" data-purecounter-end="15" data-purecounter-duration="1" class="purecounter"></span>
-            <p>Hard Workers</p>
-          </div>
+        <?php
+            $counters = $options['courser_section_repeater'];
+            foreach( $counters as $counter):
+        ?>
+            <div class="col-lg-3 col-6 text-center">
+                <span data-purecounter-start="0" data-purecounter-end="<?php echo $counter['counter_number']; ?>" data-purecounter-duration="1" class="purecounter"></span>
+                <p>
+                    <?php
+                        if( isset( $counter['counter_name'] ) ){
+                            echo $counter['counter_name'];
+                        }
+                    ?>
+                </p>
+            </div>
+        <?php
+            endforeach;
+        ?>
+          
 
         </div>
 
